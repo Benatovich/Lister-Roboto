0bf78521e96b9ac0916dff18e8847d5be8b3e503
Benatovich <sambenatovich@gmail.com>
2022-02-18 14:15:40 -0500

    MovieItem component is complete, but it the app still isn't working properly. going to finish writing all of the components before going back in to troubleshoot

--- src/MovieItem.js
+++ src/MovieItem.js
@@ -1,8 +1,31 @@
 import React from 'react';
+import { addToWatchlist } from './services/fetch-utils';
 
+export default function MovieItem(props) {
+  const haveWatched = props.isOnWatchlist(props.movie.id);
+  async function handleClick() {
+    if (!haveWatched) {
+      const watchlistItem = {
+        title: props.movie.title,
+        api_id: props.movie.id,
+        description: props.movie.overview,
+        poster: props.movie.poster_path,
+      };
 
-export default function MovieItem() {
+      await addToWatchlist(watchlistItem);
+      await props.refreshWatchlist();
+    }
+  }
+
   return (
-    <div>MovieItem</div>
+    <div onClick={handleClick} 
+      className={`movie-item ${haveWatched ? 'watched' : ''}`}>
+      <h1>{haveWatched && '❤️'}</h1>
+      <h3>{props.movie.title}</h3>
+      <em>{props.movie.overview}</em>
+      <p>
+        <img src={props.movie.poster_path ? `https://image.tmdb.org/t/p/original${props.movie.poster_path}` : './movie-poster-placeholder.png'} />
+      </p>
+    </div>
   );
 }
