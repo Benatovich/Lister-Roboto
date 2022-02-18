cb5e7de6ac4093673b803c0a737c5b39696e3255
Benatovich <sambenatovich@gmail.com>
2022-02-17 20:13:35 -0500

    authpage component should work and fetch-utils are all written

---
+++ src/services/fetch-utils.js
@@ -0,0 +1,58 @@
+import { client, checkError } from './client';
+
+export function getUser() {
+  return client.auth.session() && client.auth.session().user;
+}
+
+export async function signUpUser(email, password) {
+  const response = await client.auth.signUp({ email, password });
+
+  return response.user;
+}
+
+export async function signInUser(email, password) {
+  const response = await client.auth.signIn({ email, password });
+
+  return response.user;
+}
+
+export async function logout() {
+  await client.auth.signOut();
+
+  return (window.location.href = '../');
+}
+
+export async function addToWatchlist(movie) {
+  const response = await client
+    .from('watchlist-items')
+    .insert(movie);
+
+  return checkError(response);
+}
+
+export async function getWatchlist() {
+  const response = await client
+    .from('watchlist-items')
+    .select()
+    .order('id');
+
+  return checkError(response);
+}
+
+export async function watchMovie(id) {
+  const response = await client
+    .from('watchlist-items')
+    .update({ watched: true })
+    .match({ id })
+    .single();
+
+  return checkError(response);
+}
+
+export async function searchMovies(query) {
+  const response = await fetch(`/.netlify/functions/movies-endpoint?search=${query}`);
+
+  const json = await response.json();
+
+  return json.data.results;
+}
