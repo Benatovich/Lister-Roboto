12e820cc29dafe61394b23150c7968511310f34c
Benatovich <sambenatovich@gmail.com>
2022-02-17 20:50:22 -0500

    App.js is set up, but for some reason typed passwords are visible. looking into that

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+  NavLink,
+} from 'react-router-dom';
 import './App.css';
+import AuthPage from './AuthPage';
+import SearchPage from './SearchPage';
+import WatchlistPage from './WatchlistPage';
+import { logout } from './services/fetch-utils';
+import { useState } from 'react';
 
 function App() {
+  const [user, setUser] = useState(localStorage.getItem('supabase.auth.token'));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        {
+          user &&
+          <ul>
+            <li>
+              <NavLink activeClassName='my-active-class' to="/search">Search Page</NavLink>
+            </li>
+            <li>
+              <NavLink activeClassName='my-active-class' to="/watchlist">Watchlist</NavLink>
+            </li>
+            <li>
+              <button onClick={logout}>Logout</button>
+            </li>
+          </ul>
+        }
+        <Switch>
+          <Route exact path="/">
+            {
+              user
+                ? <Redirect to="/search"/>
+                : <AuthPage setUser={setUser}/>
+            }
+          </Route>
+          <Route exact path="/search">
+            {
+              !user
+                ? <Redirect to="/"/>
+                : <SearchPage />
+            }
+          </Route>
+          <Route exact path="/watchlist">
+            {
+              !user
+                ? <Redirect to="/"/>
+                : <WatchlistPage />
+            }
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
