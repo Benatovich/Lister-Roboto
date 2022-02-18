6c8fa01fc7c3bc2d2fe711e3e852c7293704f743
Benatovich <sambenatovich@gmail.com>
2022-02-18 17:33:15 -0500

    WatchlistItem component complete. troubleshooting now

--- src/WatchlistItem.js
+++ src/WatchlistItem.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import { watchMovie } from './services/fetch-utils';
 
-export default function WatchlistItem() {
+export default function WatchlistItem(props) {
+  async function handleClick(){
+    await watchMovie(props.movie.id);
+
+    await props.refreshWatchlist();
+  }
+
   return (
-    <div>WatchlistItem</div>
+    <div onClick={handleClick}
+      className='movie-item watchlist-item'>
+      <h1> {props.movie.watched ? '✅' : '👀'}</h1>
+      <h3>{props.movie.title}</h3>
+      <em>{props.movie.description}</em>
+      <p>
+        <img src={props.movie.poster_path ? `https://image.tmdb.org/t/p/original${props.movie.poster_path}` : './movie-poster-placeholder.png'} />
+      </p>
+    </div>
   );
 }
